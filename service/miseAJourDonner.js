c922f59aa6d7a641fe8f2475d04d916ebe3d9dfa
Gabriel Lafrance <gablafrance14@gmail.com>
2025-01-07 12:33:15 -0500

    Classe mis a jour de donner

--- service/miseAJourDonner.js
+++ service/miseAJourDonner.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 const insererUpdateJoueur = require('./InformationApi');
-// Planifier la tâche cron pour exécuter insererUpdateJoueur(); toutes les 24 heures 3h du matin
+// Planifier la tâche cron pour exécuter les fonctions qui ont besoin de mise a jour toutes les 24 heures 3h du matin
 cron.schedule('0 3 * * *', () => {
     console.log('Exécution de la tâche cron pour mettre à jour les données des joueurs');
     insererUpdateJoueur();
