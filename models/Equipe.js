f8d74d501d8d6943d8b3fff5cedb7364b4dcf2fe
Gabriel Lafrance <gablafrance14@gmail.com>
2025-01-08 13:41:35 -0500

    mis a jour gab

--- models/Equipe.js
+++ models/Equipe.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
 
 const equipeSchema = new mongoose.Schema({
-  _id: String,  // ID unique de l'équipe
-  name: { type: String, required: true },
-  logo: { type: String, required: true },
-  city: { type: String, required: true },
-  conference: { type: String, enum: ['Eastern', 'Western'], required: true },
-  division: { type: String, required: true },
-  founded: { type: Number, required: true },
-  championships: { type: Number, default: 0 },
-  statistiqueEquipe: { type: mongoose.Schema.Types.ObjectId, ref: 'Equipe', required: true }
+  _id: String, // ID unique fourni par une API ou similaire
+  teamsABV: { type: String, required: true }, // Abbréviation de l'équipe
+  name: { type: String, required: true }, // Nom de l'équipe
+  logo: { type: String, required: true }, // URL du logo
+  city: { type: String, required: true }, // Ville de l'équipe
+  conference: { type: String, enum: ['Eastern', 'Western'], required: true }, // Conférencen de l'équipe (Est ou Ouest)
+  division: { type: String, required: true }, // Division de l'équipe (ex: Atlantic, Central, etc.)
+  founded: { type: Number, required: true }, // Année de fondation
+  championships: { type: Number, default: 0 }, // Nombre de championnats
+  statistiqueEquipe: { //Récuprère les statistiques de l'équipe du model StatistiquesEquipe
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'StatistiquesEquipe', // Référence au modèle des statistiques d'équipe
+    required: true 
+  }
+
 });
 
 const Equipe = mongoose.model('Equipe', equipeSchema);
--- models/Joueur.js
+++ models/Joueur.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const joueurSchema = new mongoose.Schema({
   _id: Number,
+  headshot: { type: String, required: false },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   position: { type: String, required: true },
