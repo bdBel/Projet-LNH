4a05dcd228952da7610d78a6dae465518f038bf7
bdBel <wildvolley22@gmail.com>
2025-01-13 15:31:00 -0500

    userModel+userloginSignupRoutes+Login-signup ejs

---
+++ config/tableUser.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const User = require('../models/User'); // Import the User model
+const connectDB = require('./db');
+// Connect to MongoDB
+connectDB()
+    .then(() => {
+        console.log("Connected to MongoDB!");
+
+        // Now that we're connected, let's create the table (collection)
+        const newUser = new User({
+            nom: 'fan',
+            prenom: 'Doe',
+            password: '1111',
+            Email: 'john.doe@example.com'
+        });
+
+        newUser.save()
+            .then(() => {
+                console.log('User saved successfully!');
+                mongoose.connection.close();  // Close the connection after saving
+            })
+            .catch((err) => {
+                console.error('Error saving user:', err);
+                mongoose.connection.close();
+            });
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
