75e40e234d49dd7e060221fed50eef10498c85f4
Gabriel Lafrance <gablafrance14@gmail.com>
2025-01-16 21:37:32 -0500

    Page resultat qui affiche lorsqu'on appuie sur une partie en cours ou terminer qui montre le pointage et les but marquer

--- service/liveScoreService.js
+++ service/liveScoreService.js
@@ -1,36 +1,39 @@
 const axios = require('axios');
 
-const API_URL = 'https://api-web.nhle.com/v1/scoreboard/now';
+
 
 const getGamesLive = async () => {
+    const API_URL = 'https://api-web.nhle.com/v1/scoreboard/now';
     try {
         const response = await axios.get(API_URL);
         const data = response.data;
-        
-        // Transformer les données si nécessaire
+
+        // Obtenez la date actuelle au format "YYYY-MM-DD"
+        const today = new Date().toISOString().split('T')[0];
+
+        // Transformer les données
         const gamesByDate = data.gamesByDate.map(dateEntry => ({
-            date: dateEntry.date,
+            date: dateEntry.date, // Date au format "YYYY-MM-DD"
             games: dateEntry.games.map(game => {
-                
+
                 // Formatage du startTimeUTC
                 const startTime = new Date(game.startTimeUTC);
                 const AmPM = startTime.getHours() >= 12 ? ' PM' : ' AM';
                 const formattedTime = (startTime.getHours() < 10 ? '0' : '') + startTime.getHours() + ':' + (startTime.getMinutes() < 10 ? '0' : '') + startTime.getMinutes() + AmPM;
-                const day = startTime.getDate();
-                
+                const today = startTime.getDate();
+
                 return {
                     id: game.id,
                     gameState: game.gameState,
+                    recapLink: game.threeMinRecap,
                     date: game.gameDate,
                     status: game.status,
                     startTime: game.startTimeUTC,
                     formattedStartTime: formattedTime,  // Heure formatée
-                    day: day,  // Jour formaté
+                    day: today,  // Jour formaté
                     period: game.period,
                     periodDescriptor: {
                         number: game.periodDescriptor
-                        //periodType: game.periodDescriptor.periodType,
-                        //maxRegulationPeriods: game.periodDescriptor
                     },
                     homeTeam: {
                         name: game.homeTeam.name.default,
@@ -46,10 +49,7 @@ const getGamesLive = async () => {
                     }
                 };
             })
-        }));
-
-        
-        
+        }));      
         return gamesByDate;
     } catch (error) {
         console.error('Erreur lors de la récupération des données:', error.message);
@@ -57,6 +57,21 @@ const getGamesLive = async () => {
     }
 };
 
-module.exports = { getGamesLive };
+const getSumary = async (id) => {
+    const API_URL = `https://api-web.nhle.com/v1/gamecenter/${id}/landing`;
+    
+    try {
+        const response = await axios.get(API_URL);
+        const data = response.data;
+        return data;
+    } catch (error) {
+        console.error('Erreur lors de la récupération des données: dans le service ', error.message);
+        throw new Error('Impossible de récupérer les données des matchs. dans le service');
+    }
+};
 
 
+
+
+
+module.exports = { getGamesLive,getSumary};
